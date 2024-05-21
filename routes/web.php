e86006d6bfbbd968725dff356fe7453508c8da3c
mohmaruf <mohmaruf912@gmail.com>
2024-05-21 22:03:20 +0800

    menambah halaman login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landingpages/master');
 });
+
+Route::get('/login', [AuthController::class, 'index']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
